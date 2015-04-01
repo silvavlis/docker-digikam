00c595d04cc2cea8918dde7cb6b0d11938aec88a
Silvano Cirujano Cuesta <silvanociru@gmx.net>
2015-04-01 22:13:49 +0200

    Update to Docker Hub image name
    Change image name to fit that of the Docker Hub Registry.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build --tag silvano/digikam .
+docker build --tag silvavlis/digikam .
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --user user-dk --name digikam -it -v ${HOME}/Pictures/docker-digikam:/home/user-dk/digikam -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY silvano/digikam
+docker run --user user-dk --name digikam -it -v ${HOME}/Pictures/docker-digikam:/home/user-dk/digikam -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY silvavlis/digikam
