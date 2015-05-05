807a41db8e0d8f3449a88b70b5447d7c7f20d638
Silvano Cirujano Cuesta <silvanociru@gmx.net>
2015-05-05 23:04:05 +0200

    Base container on latest image
    Run container not based on a fixed image version, but on the latest one.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --user user-dk --name digikam-4.9 -it -v ${HOME}/Pictures/digikam:/home/user-dk/Pictures -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY silvavlis/digikam:4.9
+docker run --user user-dk --name digikam -it -v ${HOME}/Pictures/digikam:/home/user-dk/Pictures -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY silvavlis/digikam
