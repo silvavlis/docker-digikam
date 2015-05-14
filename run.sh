f3bd0dfd01dd8bb1e27e6f058733fb4d719a724b
Silvano Cirujano Cuesta <silvanociru@gmx.net>
2015-05-14 21:51:02 +0200

    Refactor container execution
    Use easier to understand "if else" condition instead of default execution path.

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 if [ ! -z `docker ps --all=true --quiet --filter=name=digikam` ] ; then
     docker start digikam
-    exit 0
+else
+    docker run --user user-dk --name digikam -it -v ${HOME}/Pictures/digikam:/home/user-dk/Pictures -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY silvavlis/digikam
 fi
-docker run --user user-dk --name digikam -it -v ${HOME}/Pictures/digikam:/home/user-dk/Pictures -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY silvavlis/digikam
