919ae15a11b7661fa883360e4af174af517f4b1b
Silvano Cirujano Cuesta <silvanociru@gmx.net>
2015-05-14 21:52:04 +0200

    Improve script verbosity
    Informing if container being directly started or being created before.

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if [ ! -z `docker ps --all=true --quiet --filter=name=digikam` ] ; then
+    echo "Container 'digikam' already exists, starting it"
     docker start digikam
 else
+    echo "Container 'digikam' doesn't exist yet, creating and starting it"
     docker run --user user-dk --name digikam -it -v ${HOME}/Pictures/digikam:/home/user-dk/Pictures -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY silvavlis/digikam
 fi
