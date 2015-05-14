fbc224a18502d7e7b0eec885e1a5a9bc7e0fe106
Silvano Cirujano Cuesta <silvanociru@gmx.net>
2015-05-14 21:56:28 +0200

    Refactor execution script
    Disable usage of double negative logic in script for container execution.

--- run.sh
+++ run.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-if [ ! -z `docker ps --all=true --quiet --filter=name=digikam` ] ; then
-    echo "Container 'digikam' already exists, starting it"
-    docker start digikam
-else
+if [ -z `docker ps --all=true --quiet --filter=name=digikam` ] ; then
     echo "Container 'digikam' doesn't exist yet, creating and starting it"
     docker run \
         -it \
@@ -13,4 +10,7 @@ else
         -v /tmp/.X11-unix:/tmp/.X11-unix \
         -e DISPLAY=$DISPLAY \
         silvavlis/digikam
+else
+    echo "Container 'digikam' already exists, starting it"
+    docker start digikam
 fi
